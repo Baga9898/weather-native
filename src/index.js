ea9d3f5a716aff0e70abd7099444a9f3e217c567
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-09-07 22:38:14 +0300

    Add linear gradient background

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import {
     Dimensions,
     FlatList,
 } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const openWeatherKey = '874d0af8e952eb0b39aa7aa85cefabf8';
 let url = `http://api.openweathermap.org/data/2.5/onecall?&units=metric&exclude=minutely&appid=${openWeatherKey}`;
@@ -59,106 +60,113 @@ const Weather = () => {
     const current = forecast.current.weather[0];
 
   return (
-    <SafeAreaView style={styles.container}>
-        <ScrollView 
-            refreshControl={
-                <RefreshControl 
-                refreshing={refreshing} 
-                onRefresh={() => loadForecast()} 
-                />
-            }
-            style={{marginTop: 50}}
-        >
-            <Text style={styles.title}>
-                Current Weather
-            </Text>
-            <Text style={{alignItems: 'center', textAlign: 'center'}}>
-                Your Location
-            </Text>
-            <View style={styles.current}>
-                <Image
-                    style={styles.largeIcon}
-                    source={{
-                        uri: `http://openweathermap.org/img/wn/${current.icon}@4x.png`,
+    <LinearGradient
+        colors={['#4681c9', '#364699']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={{ flex:1 }}
+    >
+        <SafeAreaView style={styles.container}>
+            <ScrollView 
+                refreshControl={
+                    <RefreshControl 
+                    refreshing={refreshing} 
+                    onRefresh={() => loadForecast()} 
+                    />
+                }
+                style={{marginTop: 50}}
+            >
+                <Text style={styles.title}>
+                    Current Weather
+                </Text>
+                <Text style={{alignItems: 'center', textAlign: 'center'}}>
+                    Your Location
+                </Text>
+                <View style={styles.current}>
+                    <Image
+                        style={styles.largeIcon}
+                        source={{
+                            uri: `http://openweathermap.org/img/wn/${current.icon}@4x.png`,
+                        }}
+                    />
+                    <Text style={styles.currentTemp}>
+                        {Math.round(forecast.current.temp)}℃
+                    </Text>
+                </View>
+
+                <Text style={styles.currentDescription}>
+                    {current.description}
+                </Text>
+
+                <View style={styles.extraInfo}>
+                    <View style={styles.info}>
+                        <Image
+                            source={require('../assets/temp.png')}
+                            style={{width: 40, height: 40, borderRadius: 40/2, marginLeft: 50}}
+                        />
+                        <Text style={styles.text}>
+                            {forecast.current.feels_like}℃
+                        </Text>
+                        <Text style={styles.text}>
+                            Feells like
+                        </Text>
+                    </View>
+                    <View style={styles.info}>
+                        <Image
+                            source={require('../assets/humidity.png')}
+                            style={{width: 40, height: 40, borderRadius: 40/2, marginLeft: 50}}
+                        />
+                        <Text style={styles.text}>
+                            {forecast.current.humidity}%
+                        </Text>
+                        <Text style={styles.text}>
+                            Humidity
+                        </Text>
+                    </View>
+                </View>
+
+                <View>
+                    <Text style={styles.subtitle}>Hourly forecast</Text>
+                </View>
+
+                <FlatList
+                    horizontal
+                    data={forecast.hourly.slice(0, 24)}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={(hour) => {
+                        const weather = hour.item.weather[0];
+                        let dt = new Date(hour.item.dt * 1000);
+                        return (
+                            <View style={styles.hour}>
+                                <Text style={{fontWeight:'bold', color:'#346751'}}>
+                                    {dt.toLocaleTimeString().replace(/:\d+ /, ' ')}
+                                </Text>
+                                <Text>
+                                    {Math.round(hour.item.temp)}℃
+                                </Text>
+                                <Image
+                                    style={styles.smallIcon}
+                                    source={{
+                                        uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`
+                                    }}
+                                />
+                                <Text style={{fontWeight: 'bold', color: '#346751'}}>
+                                    {weather.description}
+                                </Text>
+                            </View>
+                        );
                     }}
                 />
-                <Text style={styles.currentTemp}>
-                    {Math.round(forecast.current.temp)}℃
-                </Text>
-            </View>
-
-            <Text style={styles.currentDescription}>
-                {current.description}
-            </Text>
-
-            <View style={styles.extraInfo}>
-                <View style={styles.info}>
-                    <Image
-                        source={require('../assets/temp.png')}
-                        style={{width: 40, height: 40, borderRadius: 40/2, marginLeft: 50}}
-                    />
-                    <Text style={styles.text}>
-                        {forecast.current.feels_like}℃
-                    </Text>
-                    <Text style={styles.text}>
-                        Feells like
-                    </Text>
-                </View>
-                <View style={styles.info}>
-                    <Image
-                        source={require('../assets/humidity.png')}
-                        style={{width: 40, height: 40, borderRadius: 40/2, marginLeft: 50}}
-                    />
-                    <Text style={styles.text}>
-                        {forecast.current.humidity}%
-                    </Text>
-                    <Text style={styles.text}>
-                        Humidity
-                    </Text>
-                </View>
-            </View>
-
-            <View>
-                <Text style={styles.subtitle}>Hourly forecast</Text>
-            </View>
-
-            <FlatList
-                horizontal
-                data={forecast.hourly.slice(0, 24)}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={(hour) => {
-                    const weather = hour.item.weather[0];
-                    let dt = new Date(hour.item.dt * 1000);
-                    return (
-                        <View style={styles.hour}>
-                            <Text style={{fontWeight:'bold', color:'#346751'}}>
-                                {dt.toLocaleTimeString().replace(/:\d+ /, ' ')}
-                            </Text>
-                            <Text>
-                                {Math.round(hour.item.temp)}℃
-                            </Text>
-                            <Image
-                                style={styles.smallIcon}
-                                source={{
-                                    uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`
-                                }}
-                            />
-                            <Text style={{fontWeight: 'bold', color: '#346751'}}>
-                                {weather.description}
-                            </Text>
-                        </View>
-                    );
-                }}
-            />
-        </ScrollView>
-    </SafeAreaView>
+            </ScrollView>
+        </SafeAreaView>
+    </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ecdbba',
+        // backgroundColor: '#1a6cd4',
     },
     title: {
         textAlign: 'center',
