2cdc003db6f76a0d1b87f6c3890927ccfe80849a
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-09-08 21:48:23 +0300

    Add test photo

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import {
 } from 'react-native';
 
 const openWeatherKey = '874d0af8e952eb0b39aa7aa85cefabf8';
-let url = `http://api.openweathermap.org/data/2.5/onecall?&units=metric&exclude=minutely&appid=${openWeatherKey}`;
+let url = `http://api.openweathermap.org/data/2.5/onecall?&units=metric&exclude=minutely&lang=ru&appid=${openWeatherKey}`;
 
 const Weather = () => {
     const [forecast, setForecast] = useState(null);
@@ -58,6 +58,7 @@ const Weather = () => {
     }
 
     const current = forecast.current.weather[0];
+    const feelsLike = 'Ощущается ';
 
   return (
     <LinearGradient
@@ -76,41 +77,37 @@ const Weather = () => {
                 }
                 style={{marginTop: 50}}
             >
-                <Text style={styles.title}>
+                {/* <Text style={styles.title}>
                     Current Weather
                 </Text>
                 <Text style={{alignItems: 'center', textAlign: 'center'}}>
                     Your Location
-                </Text>
+                </Text> */}
                 <View style={styles.current}>
+                    <Text style={styles.currentTemp}>
+                        {Math.round(forecast.current.temp)}°
+                    </Text>
+                    {/* <Image 
+                            source={require('../assets/sunny.png')}
+                        /> */}
                     <Image
                         style={styles.largeIcon}
-                        source={{
-                            uri: `http://openweathermap.org/img/wn/${current.icon}@4x.png`,
-                        }}
+                        source={require('../assets/soCold.png')}
+                        // source={{
+                        //     uri: `http://openweathermap.org/img/wn/${current.icon}@4x.png`,
+                        // }}
                     />
-                    <Text style={styles.currentTemp}>
-                        {Math.round(forecast.current.temp)}℃
-                    </Text>
                 </View>
 
                 <Text style={styles.currentDescription}>
                     {current.description}
                 </Text>
+                <Text style={styles.feelsLike}>
+                    {feelsLike}
+                    {Math.round(forecast.current.feels_like)}°
+                </Text>
 
-                <View style={styles.extraInfo}>
-                    <View style={styles.info}>
-                        <Image
-                            source={require('../assets/temp.png')}
-                            style={{width: 40, height: 40, borderRadius: 40/2, marginLeft: 50}}
-                        />
-                        <Text style={styles.text}>
-                            {forecast.current.feels_like}℃
-                        </Text>
-                        <Text style={styles.text}>
-                            Feells like
-                        </Text>
-                    </View>
+                {/* <View style={styles.extraInfo}>
                     <View style={styles.info}>
                         <Image
                             source={require('../assets/humidity.png')}
@@ -123,11 +120,11 @@ const Weather = () => {
                             Humidity
                         </Text>
                     </View>
-                </View>
+                </View> */}
 
-                <View>
-                    <Text style={styles.subtitle}>Hourly forecast</Text>
-                </View>
+                {/* <View>
+                    <Text style={styles.subtitle}>Почасовой прогноз</Text>
+                </View> */}
 
                 <FlatList
                     horizontal
@@ -142,17 +139,15 @@ const Weather = () => {
                                     {dt.toLocaleTimeString().replace(/:\d+ /, ' ')}
                                 </Text>
                                 <Text style={{color: '#fff'}}>
-                                    {Math.round(hour.item.temp)}℃
+                                    {Math.round(hour.item.temp)}°
                                 </Text>
                                 <Image
-                                    style={styles.smallIcon}
-                                    source={{
-                                        uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`
-                                    }}
+                                    style={styles.hourlyIcon}
+                                    source={require('../assets/sunny.png')}
                                 />
-                                <Text style={{fontWeight: 'bold', color: '#fff'}}>
+                                {/* <Text style={{fontWeight: 'bold', color: '#fff'}}>
                                     {weather.description}
-                                </Text>
+                                </Text> */}
                             </View>
                         );
                     }}
@@ -165,7 +160,7 @@ const Weather = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        // flex: 1,
     },
     title: {
         textAlign: 'center',
@@ -182,16 +177,23 @@ const styles = StyleSheet.create({
         height: 250,
     },
     currentTemp: {
-        fontSize: 35,
+        fontSize: 55,
         fontWeight: 'bold',
         textAlign: 'center',
+        color: '#fff',
+        marginLeft: 30,
     },
     currentDescription: {
         width: '100%',
-        textAlign: 'center',
-        fontWeight: '200',
+        fontWeight: '400',
         fontSize: 24,
         marginBottom: 5,
+        marginLeft: 30,
+        color: '#fff',
+    },
+    feelsLike: {
+        color: '#fff',
+        marginLeft: 30,
     },
     info: {
         width: Dimensions.get('screen').width/2.5,
@@ -217,10 +219,15 @@ const styles = StyleSheet.create({
         marginLeft: 7,
         color: '#fff',
         fontWeight: 'bold',
+        marginLeft: 30,
     },
     hour: {
         padding: 6,
         alignItems: 'center',
+    },
+    hourlyIcon: {
+        with: 50,
+        height: 50,
     },
     smallIcon: {
         with: 100,
